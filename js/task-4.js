079acfbb3a33f68563093f0c6d0d978badc7e244
Will IM <vitalii.mieliet@gmail.com>
2025-04-18 17:24:04 +0300

    Do 4th task

--- js/task-4.js
+++ js/task-4.js
@@ -10,6 +10,12 @@
 'use strict';
 console.log('\nTask-4\n');
 
+const getTotalBalanceByGender = (users, gender) => {
+  return users
+    .filter(user => user.gender === gender)
+    .reduce((totalBalance, user) => (totalBalance += user.balance), 0);
+};
+
 const clients = [
   {
     name: 'Moore Hensley',
